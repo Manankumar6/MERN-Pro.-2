3424ccee1b0a8f0fc0462897b5a72da7aa4605a1
Manankumar6 <manankumar2019@gmail.com>
2024-02-17 20:57:32 +0530

    update json file

--- server/server.js
+++ server/server.js
@@ -9,13 +9,9 @@ const serviceRoute = require('./router/service-router')
 const adminRoute = require('./router/admin-router')
 const connectDb = require('./utils/db');
 const errorMiddleware = require("./middleware/error_middleware");
-// let's tackle cors
-const corsOptions = {
-    origin: 'http://localhost:3000',
-   methods: "GET,POST,PUT,DELETE, PATCH,HEAD",
-   credential:true
-  }
-app.use(cors(corsOptions));
+// let's tackle cor
+
+app.use(cors());
 app.use(express.json())
 app.use("/api/auth",authRouter);
 app.use("/api/form",contactRoute)
